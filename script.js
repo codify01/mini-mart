00503166039cd0c59312801c1b32e44e3b061818
Uthman Oluwaseun <amoleuthman@gmail.com>
2024-01-11 12:38:03 +0100

    second commit

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ function displayCart() {
     for (i = 0; i < cart.length; i++) {
         show.innerHTML += `<tr> <th class="text-center">${i+1}.</th class="text-center"> <td>${cart[i]}</td></tr>`
     }
+    cartNum.innerHTML = cart.length
+}
+function displayMessage() {
+    if (cart.length = 0) {
+        show.innerHTML = `<td colspan="2" class="text-center fw-bold fs-4">Add something to your cart</td>`
+    }
 }
 function addItems() {
+    var co
     if (inp.value === "") {
         error.style.display = "block"
         // alert("Fill in something now")
@@ -13,41 +20,43 @@ function addItems() {
         cart.push(inp.value)
         inp.value = ''
         error.style.display = "none"
-        console.log(cart);
+        // console.log(cart);
+        // cartNum.innerHTML = cart.length
        displayCart()
     }
     inp.focus()
 }
 function frontAdd() {
     if (inp.value === "") {
-        alert("Fill in something now")
+        error.style.display = "block"
     }else {
         cart.unshift(inp.value)
         inp.value = ''
-        
-        console.log(cart);
-        displayCart
+        error.style.display = "none"
+        // console.log(cart);
+
+        displayCart()
     }
     inp.focus()
 }
 
 function backSpace() {   
-    cart.pop(show.innerHTML)
-    // show.innerHTML = cart
-    displayCart()
+        cart.pop(show.innerHTML)
+        displayCart()
 }
 function frontSpace() {   
     cart.shift(show.innerHTML)
     // show.innerHTML = cart
-   displayCart
+   displayCart()
 }
 function deleteAll() {
-    show.innerHTML = ''
+    show.innerHTML = `<td colspan="2" class="text-center text-success fw-bold fs-4">Cart cleared successfully</td>`
     cart.length = 0
+    cartNum.innerHTML = cart.length
 }
 function edit() {
     var itemNumber = prompt('Enter item number')
-    if (itemNumber > cart.length && isNaN(itemNumber)) {
+    if (itemNumber > cart.length) {
         alert('Invalid number')
     }else {
         var newItem = prompt('Enter new item name')
