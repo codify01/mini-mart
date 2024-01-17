8151615e426e9a8de735322a19078d09b0af944d
Uthman Oluwaseun <amoleuthman@gmail.com>
2024-01-17 09:13:54 +0100

    Updated the version

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var cart = []
 function displayCart() {
     show.innerHTML = ""
     for (i = 0; i < cart.length; i++) {
-        show.innerHTML += `<tr> <th class="text-center">${i+1}.</th class="text-center"> <td>${cart[i]}</td></tr>`
+        show.innerHTML += `<tr> <th class="text-center">${i+1}.</th class="text-center"> <td>${cart[i]} </td><td><button class="btn btn-danger btn-sm" onclick="del(${i})">Delete</button></td><td><button class="btn btn-warning btn-sm" onclick="edit(${i})">Edit</button></td></tr>`
     }
     cartNum.innerHTML = cart.length
 }
@@ -26,53 +26,57 @@ function addItems() {
     }
     inp.focus()
 }
-function frontAdd() {
-    if (inp.value === "") {
-        error.style.display = "block"
-    }else {
-        cart.unshift(inp.value)
-        inp.value = ''
-        error.style.display = "none"
-        // console.log(cart);
-
-        displayCart()
-    }
-    inp.focus()
+function del(index) {
+    cart.splice(index, 1)
+    displayCart()
 }
+// function frontAdd() {
+//     if (inp.value === "") {
+//         error.style.display = "block"
+//     }else {
+//         cart.unshift(inp.value)
+//         inp.value = ''
+//         error.style.display = "none"
+//         // console.log(cart);
+
+//         displayCart()
+//     }
+//     inp.focus()
+// }
 
 function backSpace() {   
         cart.pop(show.innerHTML)
         displayCart()
 }
-function frontSpace() {   
-    cart.shift(show.innerHTML)
-    // show.innerHTML = cart
-   displayCart()
-}
+// function frontSpace() {   
+//     cart.shift(show.innerHTML)
+//     // show.innerHTML = cart
+//    displayCart()
+// }
 function deleteAll() {
-    show.innerHTML = `<td colspan="2" class="text-center text-success fw-bold fs-4">Cart cleared successfully</td>`
+    if (cart.length != 0) {
+        show.innerHTML = `<td colspan="2" class="text-center text-success fw-bold fs-4">Cart cleared successfully</td>`
     cart.length = 0
     cartNum.innerHTML = cart.length
+    } else {
+        alert("You have nothing to clear")
+    }
 }
-function edit() {
-    var itemNumber = prompt('Enter item number')
-    if (itemNumber > cart.length) {
-        alert('Invalid number')
-    }else {
+function edit(value) {
+    // var itemNumber = prompt('Enter item number')
         var newItem = prompt('Enter new item name')
-        cart.splice(itemNumber-1, 1, newItem)
+        cart.splice(value, 1, newItem)
         displayCart()
-    }
 }
-function deleteAny() {
-    var itemNumber = Number(prompt('Enter item number'))
-    if (itemNumber > cart.length) {
-        alert('Invalid number')
-    }else {
-        cart.splice(itemNumber-1, 1)
-        displayCart()
-    }
-}
+// function deleteAny() {
+//     var itemNumber = Number(prompt('Enter item number'))
+//     if (itemNumber > cart.length) {
+//         alert('Invalid number')
+//     }else {
+//         cart.splice(itemNumber-1, 1)
+//         displayCart()
+//     }
+// }
 
 
 
